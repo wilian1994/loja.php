8b238670fcdc7825a1b95d3a0d14240830ab24d2
Wilian da Silva Oliveira <wsilva@cleartech.com.br>
2018-02-09 09:56:45 -0200

    Criado a conexão com o banco

--- adiciona-produto.php
+++ adiciona-produto.php
@@ -1,18 +1,27 @@
-<?php include("cabecalho.php"); ?>
-
 <?php
+include("cabecalho.php");
+
+function insereProduto($conexao, $nome, $preco){
+    $query = "insert into produtos (nome, preco) values ('{$nome}', {$preco})";
+    return mysqli_query($conexao, $query);
+}
+
+
 $nome = $_GET["nome"];
 $preco = $_GET["preco"];
+
+
 $conexao = mysqli_connect('localhost', 'root', '', 'loja');
 
-$query = "insert into produtos (nome, preco) values ("{$nome}", ${preco})";
-mysqli_query($conexao, $query);
-
-mysqli_close($conexao);
-
+if(insereProduto($conexao, $nome, $preco)) {
 ?>
-<p class="alert-success">
-    Produto <?= $nome; ?>, <?= $preco; ?> adicionado com sucesso!
-</p>
+<p class="text-success">Produto <?= $nome; ?>, <?= $preco; ?> adicionado com sucesso!</p>
+<?php
+} else {
+?>
+<p class="text-danger">O produto <? = $nome; ?> não foi adicionado</p>
+<?php
+}
+?>
 
 <?php include("rodape.php"); ?>
